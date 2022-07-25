90a4245f5bbefa094f85a0457f365fadf02a51ba
Vincent MIGOT <vincent.migot@orange.fr>
2022-07-26 00:29:32 +0200

    8.Requêtes SQL TP_Instagram / final git commit

--- TP-Instagram.sql
+++ TP-Instagram.sql
@@ -50,3 +50,18 @@ ALTER TABLE Users MODIFY avatar VARCHAR(10000);
 ALTER TABLE Post MODIFY url VARCHAR(10000);
 ALTER TABLE Comment DROP COLUMN nb_like;
 ALTER TABLE Post DROP COLUMN nb_like;
+
+SELECT Comment.msg, Users.username FROM Comment INNER JOIN Users ON Comment.user_id=Users.id;
+SELECT Comment.msg, Post.url FROM Comment INNER JOIN Post ON Comment.post_id=Post.id;
+SELECT Post.url, Users.username FROM Post INNER JOIN Users ON Post.user_id=Users.id;
+SELECT Comment.msg, Users.username FROM Comment INNER JOIN Users ON Comment.user_id=Users.id WHERE Comment.post_id=48;
+SELECT Users.id FROM Post INNER JOIN Users ON Post.user_id=Users.id WHERE Post.id=48;
+
+SELECT Users.username, Comment.msg, Post.url FROM Users INNER JOIN Comment ON Users.id=Comment.user_id INNER JOIN Post ON Comment.post_id=Post.id WHERE Users.id=(SELECT Users.id FROM Post INNER JOIN Users ON Post.user_id=Users.id WHERE Post.id=48);
+SELECT Users.username, Post.url, Comment.msg FROM Users INNER JOIN Post ON Users.id=Post.user_id INNER JOIN Comment ON Post.id=Comment.post_id WHERE Users.id=(SELECT Users.id FROM Post INNER JOIN Users ON Post.user_id=Users.id WHERE Post.id=48);
+
+SELECT Users.username, Post.url, Comment.msg FROM Comment
+INNER JOIN Users ON Comment.user_id=Users.id
+INNER JOIN Post ON Post.user_id=Comment.user_id WHERE Users.id=(SELECT Users.id FROM Post INNER JOIN Users ON Post.user_id=Users.id WHERE Post.id=48) AND Post.id=Comment.post_id;
+
+SELECT COUNT(post_id) FROM LikePost WHERE LikePost.post_id=39;
